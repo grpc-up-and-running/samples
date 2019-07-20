73839a40f11f4c78e6dfe0fef362253953923f6f
Danesh Kuruppu <dknkuruppu@gmail.com>
2019-07-20 18:09:08 +0530

    Add basic-authentication samples to chapter 05

---
+++ ch05/basic-authentication/go/client/main.go
@@ -0,0 +1,83 @@
+// Go to ${grpc-up-and-running}/samples/ch02/productinfo
+// Optional: Execute protoc --go_out=plugins=grpc:golang/product_info product_info.proto
+// Execute go get -v github.com/grpc-up-and-running/samples/ch02/productinfo/golang/product_info
+// Execute go run go/client/main.go
+
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"google.golang.org/grpc/credentials"
+	"log"
+	"path/filepath"
+	"time"
+
+	wrapper "github.com/golang/protobuf/ptypes/wrappers"
+	pb "github.com/grpc-up-and-running/samples/ch02/productinfo/go/product_info"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+
+	creds, err := credentials.NewClientTLSFromFile(filepath.Join("ch05", "secure-channel", "certs", "server.crt"),
+		"localhost")
+	if err != nil {
+		log.Fatalf("failed to load credentials: %v", err)
+	}
+	opts := []grpc.DialOption{
+		grpc.WithPerRPCCredentials(basicAuth{
+			username: "admin",
+			password: "admin",
+		}),
+		// transport credentials.
+		grpc.WithTransportCredentials(creds),
+	}
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, opts...)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewProductInfoClient(conn)
+
+	// Contact the server and print out its response.
+	name := "Sumsung S10"
+	description := "Samsung Galaxy S10 is the latest smart phone, launched in February 2019"
+	price := float32(700.0)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.AddProduct(ctx, &pb.Product{Name: name, Description: description, Price: price})
+	if err != nil {
+		log.Fatalf("Could not add product: %v", err)
+	}
+	log.Printf("Product ID: %s added successfully", r.Value)
+
+	product, err := c.GetProduct(ctx, &wrapper.StringValue{Value: r.Value})
+	if err != nil {
+		log.Fatalf("Could not get product: %v", err)
+	}
+	log.Printf("Product: ", product.String())
+}
+
+type basicAuth struct {
+	username string
+	password string
+}
+
+func (b basicAuth) GetRequestMetadata(ctx context.Context, in ...string) (map[string]string, error) {
+	auth := b.username + ":" + b.password
+	enc := base64.StdEncoding.EncodeToString([]byte(auth))
+	return map[string]string{
+		"authorization": "Basic " + enc,
+	}, nil
+}
+
+func (b basicAuth) RequireTransportSecurity() bool {
+	return true
+}
