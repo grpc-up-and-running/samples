257cd2548ad6bb4c025c81cf0d9c8a90c287676b
kasun04 <kasun147@gmail.com>
2019-08-08 21:46:54 -0700

    Add gRPC Cancellation sample

--- ch05/cancellation/order-service/go/client/main.go
+++ ch05/cancellation/order-service/go/client/main.go
@@ -5,7 +5,7 @@ import (
 	wrapper "github.com/golang/protobuf/ptypes/wrappers"
 	pb "github.com/grpc-up-and-running/samples/ch05/inteceptors/order-service/go/order-service-gen"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/status"
+	"io"
 	"log"
 	"time"
 )
@@ -23,34 +23,34 @@ func main() {
 	defer conn.Close()
 	client := pb.NewOrderManagementClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 5)
 	defer cancel()
 
 	// Add Order
-	order1 := pb.Order{Id: "101", Items:[]string{"iPhone XS", "Mac Book Pro"}, Destination:"San Jose, CA", Price:2300.00}
-	res, addErr := client.AddOrder(ctx, &order1)
+	//order1 := pb.Order{Id: "101", Items:[]string{"iPhone XS", "Mac Book Pro"}, Destination:"San Jose, CA", Price:2300.00}
+	//res, addErr := client.AddOrder(ctx, &order1)
+	//
+	//if addErr != nil {
+	//	got := status.Code(addErr)
+	//	log.Printf("Error Occured -> addOrder : , %v:", got)
+	//} else {
+	//	log.Print("AddOrder Response -> ", res.Value)
+	//}
 
-	if addErr != nil {
-		got := status.Code(addErr)
-		log.Printf("Error Occured -> addOrder : , %v:", got)
-	} else {
-		log.Print("AddOrder Response -> ", res.Value)
-	}
 
-
-	log.Println("Cancelling context... ")
-	cancel()
+	//log.Println("Cancelling context... ")
+	//cancel()
 
 	// Following RPC calls should fail because the RPC context is already cancelled. 
 
 	// Get Order
-	retrievedOrder , getOrderErr := client.GetOrder(ctx, &wrapper.StringValue{Value: "106"})
-
-	if getOrderErr != nil {
-		log.Printf("Error Occured -> getOrder : , %v:", status.Code(getOrderErr))
-	} else {
-		log.Print("GetOrder Response -> : ", retrievedOrder)
-	}
+	//retrievedOrder , getOrderErr := client.GetOrder(ctx, &wrapper.StringValue{Value: "106"})
+	//
+	//if getOrderErr != nil {
+	//	log.Printf("Error Occured -> getOrder : , %v:", status.Code(getOrderErr))
+	//} else {
+	//	log.Print("GetOrder Response -> : ", retrievedOrder)
+	//}
 
 
 
@@ -84,24 +84,46 @@ func main() {
 	//
 	//updateRes, _ := updateStream.CloseAndRecv()
 	//log.Printf("Update Orders Res : ", updateRes)
-	//
-	//// Process Order
-	//streamProcOrder, _ := client.ProcessOrders(ctx)
-	//_ = streamProcOrder.Send(&wrapper.StringValue{Value:"102"})
-	//_ = streamProcOrder.Send(&wrapper.StringValue{Value:"103"})
-	//_ = streamProcOrder.Send(&wrapper.StringValue{Value:"104"})
-	//
-	//channel := make(chan bool, 1)
-	//go asncClientBidirectionalRPC(streamProcOrder, channel)
-	//time.Sleep(time.Millisecond * 1000)
-	//
-	//
-	//_ = streamProcOrder.Send(&wrapper.StringValue{Value:"101"})
-	//_ = streamProcOrder.CloseSend()
-	//
-	//<- channel
+
+	// Process Order
+	streamProcOrder, _ := client.ProcessOrders(ctx)
+	_ = streamProcOrder.Send(&wrapper.StringValue{Value:"102"})
+	_ = streamProcOrder.Send(&wrapper.StringValue{Value:"103"})
+	_ = streamProcOrder.Send(&wrapper.StringValue{Value:"104"})
+
+	channel := make(chan bool, 1)
+
+
+
+	go asncClientBidirectionalRPC(streamProcOrder, channel)
+	time.Sleep(time.Millisecond * 1000)
+
+
+	log.Println("RPC Cancelled.")
+	cancel()
+	_ = streamProcOrder.Send(&wrapper.StringValue{Value:"101"})
+	_ = streamProcOrder.CloseSend()
+
+	<- channel
+
 }
 
+
+func asncClientBidirectionalRPC (streamProcOrder pb.OrderManagement_ProcessOrdersClient, c chan bool) {
+	for {
+		combinedShipment, errProcOrder := streamProcOrder.Recv()
+		if (errProcOrder != nil) {
+			log.Printf("Error Rec %v", errProcOrder)
+			break
+		} else {
+			if errProcOrder == io.EOF {
+				break
+			}
+			log.Printf("Combined shipment : ", combinedShipment.OrdersList)
+		}
+	}
+	c <- true
+}
 
 
 
