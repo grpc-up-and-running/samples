fec32fa98d00de762d16ceb3c069a49cef660372
kasun04 <kasun147@gmail.com>
2019-09-08 08:52:54 -0700

    Add gRPC ingress sample

---
+++ ch07/grpc-kubernetes/ingress/main.go
@@ -0,0 +1,48 @@
+// Go to ${grpc-up-and-running}/samples/ch02/productinfo
+// Optional: Execute protoc -I proto-gen proto-gen/product_info.proto-gen --go_out=plugins=grpc:go/product_info
+// Execute go get -v github.com/grpc-up-and-running/samples/ch02/productinfo/golang/product_info
+// Execute go run go/client/main.go
+
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	wrapper "github.com/golang/protobuf/ptypes/wrappers"
+	pb "github.com/grpc-up-and-running/samples/ch07/grpc-docker/go/proto-gen"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "productinfo:80"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewProductInfoClient(conn)
+
+	// Contact the server and print out its response.
+	name := "Sumsung S10"
+	description := "Samsung Galaxy S10 is the latest smart phone, launched in February 2019"
+	price := float32(700.0)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.AddProduct(ctx, &pb.Product{Name: name, Description: description, Price: price})
+	if err != nil {
+		log.Fatalf("Could not add product: %v", err)
+	}
+	log.Printf("Product ID: %s added successfully", r.Value)
+
+	product, err := c.GetProduct(ctx, &wrapper.StringValue{Value: r.Value})
+	if err != nil {
+		log.Fatalf("Could not get product: %v", err)
+	}
+	log.Printf("Product: ", product.String())
+}
