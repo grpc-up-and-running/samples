237d1f1fe510fe7538ab35261989abb477f6c0ec
Danesh Kuruppu <dknkuruppu@gmail.com>
2019-09-18 09:46:00 +0530

    change grpc gateway sample

--- ch08/grpc-gateway/go/client/main.go
+++ ch08/grpc-gateway/go/client/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"context"
-	"flag"
+	"log"
 	"net/http"
 
-	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 
@@ -15,10 +14,10 @@ import (
 var (
 	// command-line options:
 	// gRPC server endpoint
-	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:50051", "gRPC server endpoint")
+	grpcServerEndpoint = "localhost:50051"
 )
 
-func run() error {
+func main() {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
@@ -27,20 +26,12 @@ func run() error {
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
-	err := gw.RegisterProductInfoHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	err := gw.RegisterProductInfoHandlerFromEndpoint(ctx, mux, grpcServerEndpoint, opts)
 	if err != nil {
-		return err
+		log.Fatalf("Fail to register gRPC service endpoint: %v", err)
+		return
 	}
-
-	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	return http.ListenAndServe(":8081", mux)
-}
-
-func main() {
-	flag.Parse()
-	defer glog.Flush()
-
-	if err := run(); err != nil {
-		glog.Fatal(err)
+	if err := http.ListenAndServe(":8081", mux); err != nil {
+		log.Fatalf("Could not setup HTTP endpoint: %v", err)
 	}
 }
